4bec169de24c7508ee8d8fdc3d3a2b2d3c552fba
Weeke <baileydunlap9@gmail.com>
2023-02-07 19:04:08 -0800

    Update nmap_asyncio_syn-ack_portScan.py

--- nmap_asyncio_syn-ack_portScan.py
+++ nmap_asyncio_syn-ack_portScan.py
@@ -14,7 +14,7 @@ class NmapScanner:
         self.nm.scan(self.ip, arguments=f"-v -sS -Pn -p- -sV -g 53", callback=callback_result)
         while self.nm.still_scanning():
             print('Finishing scan!')
-            
+
         print(f"""Scan completed for IP address {self.ip}
               Host: {self.nm[self.ip].hostname()}
               OS details: {self.nm[self.ip].os_fingerprint()}""")
@@ -23,12 +23,9 @@ class NmapScanner:
         for port in self.nm[self.ip].all_tcp():
             print(f"Port {port} is {self.nm[self.ip]['tcp'][port]['state']}")
             if self.nm[self.ip].has_tcp(port):
-                service = self.nm[self.ip]['tcp'][port]['name']
-                product = self.nm[self.ip]['tcp'][port]['product']
-                version = self.nm[self.ip]['tcp'][port]['version']
-                print(f"""Service: {service}
-                      Product: {product}
-                      Version: {version}\n""")
+                print(f"""Service: {self.nm[self.ip]['tcp'][port]['name']}
+                      Product: {self.nm[self.ip]['tcp'][port]['product']}
+                      Version: {self.nm[self.ip]['tcp'][port]['version']}\n""")
 
 async def main():
     ip = input(str(f'Target: '))
